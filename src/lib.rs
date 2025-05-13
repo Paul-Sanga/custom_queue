cab13926d29b84b28ebfd82e5117334b5a9505a2
Paul Sanga Nyamawi <paul.nyamawi@griffinglobaltech.com>
2025-05-13 16:54:18 +0300

    [Basic implementation of the cutom queue datastructure]

---
+++ src/lib.rs
@@ -0,0 +1,88 @@
+use std::{
+    cell::RefCell,
+    fmt::{Debug, Display},
+    rc::{Rc, Weak},
+};
+
+mod test;
+
+#[derive(Debug)]
+pub struct Node<T> {
+    value: T,
+    next: Option<Rc<RefCell<Node<T>>>>,
+    prev: Option<Weak<RefCell<Node<T>>>>,
+}
+
+impl<T: Copy + Display + Debug + PartialEq + PartialOrd> Node<T> {
+    fn new(value: T) -> Self {
+        Self {
+            value,
+            next: None,
+            prev: None,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Queue<T> {
+    size: usize,
+    front: Option<Rc<RefCell<Node<T>>>>,
+    back: Option<Rc<RefCell<Node<T>>>>,
+}
+
+impl<T: Copy + Display + Debug + PartialEq + PartialOrd> Queue<T> {
+    pub fn new() -> Self {
+        Self {
+            size: 0,
+            front: None,
+            back: None,
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.size == 0
+    }
+
+    pub fn size(&self) -> usize {
+        self.size
+    }
+
+    pub fn front(&self) -> Result<Option<T>, String> {
+        if self.is_empty() {
+            return Err(format!("Queue is empty"));
+        }
+        Ok(self.front.as_ref().map(|node| node.borrow().value))
+    }
+
+    pub fn enqueue(&mut self, value: T) {
+        let node = Rc::new(RefCell::new(Node::new(value)));
+        if self.is_empty() {
+            self.front = Some(Rc::clone(&node));
+            self.back = Some(Rc::clone(&node));
+        }
+        self.front.as_ref().map(|front| {
+            front.borrow_mut().next = Some(Rc::clone(&node));
+            node.borrow_mut().prev = Some(Rc::downgrade(&front));
+        });
+        self.front = Some(node);
+        self.size += 1;
+    }
+
+    pub fn dequeue(&mut self)-> Result<(), String>{
+        if self.is_empty(){
+            return  Err(format!("Queue is empty"));
+        }
+        if let Some(back) = self.back.take(){
+
+            if let Some(new_back) = back.borrow_mut().next.take(){
+                self.back = Some(new_back)
+            }else{
+                self.back = None;
+                self.front = None;
+            }
+        }
+        self.size -= 1;
+        Ok(())
+    }
+
+}
---
+++ src/test.rs
@@ -0,0 +1,28 @@
+#[cfg(test)]
+
+use super::*;
+
+#[test]
+fn  test_enqueue(){
+    let mut queue: Queue<u32> = Queue::new();
+    queue.enqueue(5);
+    queue.enqueue(6);
+    assert_eq!(queue.size, 2);
+    assert_eq!(queue.front().unwrap(), Some(6));
+    dbg!(queue);   
+}
+
+#[test]
+fn test_dequeue(){
+    let mut queue: Queue<u32> = Queue::new();
+    queue.enqueue(5);
+    queue.enqueue(6);
+    assert_eq!(queue.size, 2);
+    queue.dequeue().unwrap();
+    assert_eq!(queue.front().unwrap(), Some(6));
+    assert_eq!(queue.size, 1);
+    queue.dequeue().unwrap();
+    assert_eq!(queue.front(), Err(format!("Queue is empty")));
+    assert_eq!(queue.size, 0);
+    dbg!(queue);
+}
